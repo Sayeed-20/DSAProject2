d8df12facaa3cffcd4637879c99c33e0c8db5601
Sayeed Azam <sayeed.azam838@gmail.com>
2025-11-06 22:30:56 -0500

    Trying to add initialize dataset buttons

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import tkinter as tk
-import parseSelectedData
+from parseSelectedData import parseSelectedData
 
 
 # Read dataset
@@ -133,6 +133,92 @@ def updateSelectedPropTypes(event):
 propTypeBox.bind("<<ListboxSelect>>", updateSelectedPropTypes)
 propTypeDropdown = tk.Button(window, text="Select Property Types:", command=togglePropTypeDropdown)
 propTypeDropdown.place(x=550, y=130)
+
+
+
+# Bedrooms selection
+bedroomsLabel = tk.Label(window, text="Bedrooms")
+bedroomsLabel.place(x=550, y=190)
+
+bedroomsBox = tk.Listbox(window, selectmode=tk.MULTIPLE, height=4)
+for key in selectedBedrooms:
+    if key == "68":
+        continue
+    bedroomsBox.insert(tk.END, key)
+
+def toggleBedroomsDropdown():
+    if bedroomsBox.winfo_viewable():
+        bedroomsBox.place_forget()
+    else:
+        bedroomsBox.place(x=550, y=240)
+        bedroomsBox.lift()
+
+def updateSelectedBedrooms(event):
+    selected = [bedroomsBox.get(i) for i in bedroomsBox.curselection()]
+    for key in selectedBedrooms:
+        if key in selected:
+            selectedBedrooms[key] = True
+        else:
+            selectedBedrooms[key] = False
+        print("Bedroom: ", key, "Selected: ", selectedBedrooms[key])
+
+bedroomsBox.bind("<<ListboxSelect>>", updateSelectedBedrooms)
+bedroomsDropdown = tk.Button(window, text="Select Bedrooms:", command=toggleBedroomsDropdown)
+bedroomsDropdown.place(x=550, y=210)
+
+
+# Bathrooms selection
+bathroomsLabel = tk.Label(window, text="Bathrooms")
+bathroomsLabel.place(x=550, y=270)
+
+bathroomsBox = tk.Listbox(window, selectmode=tk.MULTIPLE, height=4)
+for key in selectedBathrooms:
+    if key == "403":
+        continue
+    bathroomsBox.insert(tk.END, key)
+
+def toggleBathroomsDropdown():
+    if bathroomsBox.winfo_viewable():
+        bathroomsBox.place_forget()
+    else:
+        bathroomsBox.place(x=550, y=320)
+        bathroomsBox.lift()
+
+def updateSelectedBathrooms(event):
+    selected = [bathroomsBox.get(i) for i in bathroomsBox.curselection()]
+    for key in selectedBathrooms:
+        if key in selected:
+            selectedBathrooms[key] = True
+        else:
+            selectedBathrooms[key] = False
+        print("Bathroom: ", key, "Selected: ", selectedBathrooms[key])
+
+bathroomsBox.bind("<<ListboxSelect>>", updateSelectedBathrooms)
+bathroomsDropdown = tk.Button(window, text="Select Bathrooms:", command=toggleBathroomsDropdown)
+bathroomsDropdown.place(x=550, y=290)
+
+
+
+# Initialize data
+def initializeWithMerge():
+    print("happening")
+    selectedData = parseSelectedData(df, selectedCities, selectedPropTypes, selectedBedrooms, selectedBathrooms)
+    #call merge on selectedData
+    for i in selectedData.data:
+        print(i)
+
+def initializeWithQuick():
+    selectedData = parseSelectedData(df, selectedCities, selectedPropTypes, selectedBedrooms, selectedBathrooms)
+    #call merge on selectedData
+    for i in selectedData.data:
+        print("seen", i)
+
+mergeInitButton = tk.Button(window, text="Initialize with Merge Sort", command=initializeWithMerge)
+mergeInitButton.place(anchor = "w", x = 500, y = 250)
+
+quickInitButton = tk.Button(window, text="Initialize with Quick Sort", command=initializeWithQuick)
+quickInitButton.place(anchor = "w", x = 500, y = 350)
+
 
 window.mainloop()
 
