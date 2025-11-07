7988a7ed1692c80ce52aac24377bf84dbe1c0691
Sayeed Azam <sayeed.azam838@gmail.com>
2025-11-06 19:40:33 -0500

    Started front end development

--- src/main.py
+++ src/main.py
@@ -1,1 +1,144 @@
 import pandas as pd
+import tkinter as tk
+import parseSelectedData
+
+
+# Read dataset
+df = pd.read_csv("pakistanHousingData_cleaned.csv") # Not needed here, should be in main
+df["bedrooms"] = df["bedrooms"].astype(str)
+df["baths"] = df["baths"].astype(str)
+
+# Selection dictionaries (start with all unselected)
+selectedCities = {"Islamabad": False,
+            "Lahore": False,
+            "Faisalabad": False,
+            "Rawalpindi": False,
+            "Karachi": False}
+
+selectedPropTypes = {"Flat": False,
+                 "House": False,
+                 "Penthouse": False,
+                 "Farm House": False,
+                 "Lower Portion": False,
+                 "Upper Portion": False}
+
+selectedBedrooms = {"0": False,
+            "1": False,
+            "2": False,
+            "3": False,
+            "4": False,
+            "5": False,
+            "6": False,
+            "7": False,
+            "8": False,
+            "9": False,
+            "10": False,
+            "11": False,
+            "12": False,
+            "13": False,
+            "14": False,
+            "15": False,
+            "16": False,
+            "18": False,
+            "21": False,
+            "25": False,
+            "27": False,
+            "28": False,
+            "68": False}
+
+selectedBathrooms = {"0": False,
+             "1": False,
+             "2": False,
+             "3": False,
+             "4": False,
+             "5": False,
+             "6": False,
+             "7": False,
+             "8": False,
+             "9": False,
+             "10": False,
+             "11": False,
+             "12": False,
+             "13": False,
+             "14": False,
+             "403": False}
+
+
+
+# ----- Front End -----
+
+
+
+# Initialize Window
+window = tk.Tk()
+window.title("Pakistani Property Sorter")
+window.geometry("800x400")
+
+
+
+# City selection
+cityLabel = tk.Label(window, text="Cities")
+cityLabel.place(x=550, y=30)
+
+cityBox = tk.Listbox(window, selectmode=tk.MULTIPLE, height=4)
+for key in selectedCities:
+    cityBox.insert(tk.END, key)
+
+def toggleCityDropdown():
+    if cityBox.winfo_viewable():
+        cityBox.place_forget()
+    else:
+        cityBox.place(x=550, y=80)
+        cityBox.lift()
+
+def updateSelectedCities(event):
+    selected = [cityBox.get(i) for i in cityBox.curselection()]
+    for key in selectedCities:
+        if key in selected:
+            selectedCities[key] = True
+        else:
+            selectedCities[key] = False
+        print("key: ", key, "val: ", selectedCities[key])
+
+cityBox.bind("<<ListboxSelect>>", updateSelectedCities)
+cityDropdown = tk.Button(window, text="Select Cities:", command=toggleCityDropdown)
+cityDropdown.place(x=550, y=50)
+
+
+
+# Property Type selection
+propTypeLabel = tk.Label(window, text="Property Types")
+propTypeLabel.place(x=550, y=110)
+
+propTypeBox = tk.Listbox(window, selectmode=tk.MULTIPLE, height=4)
+for key in selectedPropTypes:
+    propTypeBox.insert(tk.END, key)
+
+def togglePropTypeDropdown():
+    if propTypeBox.winfo_viewable():
+        propTypeBox.place_forget()
+    else:
+        propTypeBox.place(x=550, y=160)
+        propTypeBox.lift()
+
+def updateSelectedPropTypes(event):
+    selected = [propTypeBox.get(i) for i in propTypeBox.curselection()]
+    for key in selectedPropTypes:
+        if key in selected:
+            selectedPropTypes[key] = True
+        else:
+            selectedPropTypes[key] = False
+        print("key: ", key, "val: ", selectedPropTypes[key])
+
+propTypeBox.bind("<<ListboxSelect>>", updateSelectedPropTypes)
+propTypeDropdown = tk.Button(window, text="Select Property Types:", command=togglePropTypeDropdown)
+propTypeDropdown.place(x=550, y=130)
+
+window.mainloop()
+
+
+
+
+
+
+
--- src/parseSelectedData.py
+++ src/parseSelectedData.py
@@ -1,107 +1,81 @@
 import pandas as pd
 from dataset import Dataset
 
-# All False dictionaries:
-cities = {"Islamabad": False,
-            "Lahore": False,
-            "Faisalabad": True,
-            "Rawalpindi": False,
-            "Karachi": True}
+# # All False dictionaries:
+# cities = {"Islamabad": False,
+#             "Lahore": False,
+#             "Faisalabad": True,
+#             "Rawalpindi": False,
+#             "Karachi": True}
+# #
+# # propertyTypes = {"Flat": False,
+# #                  "House": False,
+# #                  "Penthouse": False,
+# #                  "Farm House": False,
+# #                  "Lower Portion": False,
+# #                  "Upper Portion": False}
+# #
+# # bedrooms = {"0": False,
+# #             "1": False,
+# #             "2": False,
+# #             "3": False,
+# #             "4": False,
+# #             "5": False,
+# #             "6": False,
+# #             "7": False,
+# #             "8": False,
+# #             "9": False,
+# #             "10": False,
+# #             "11": False,
+# #             "12": False,
+# #             "13": False,
+# #             "14": False,
+# #             "15": False,
+# #             "16": False,
+# #             "18": False,
+# #             "21": False,
+# #             "25": False,
+# #             "27": False,
+# #             "28": False,
+# #             "68": False}
+# #
+# bathrooms = {"0": True,
+#              "1": False,
+#              "2": False,
+#              "3": False,
+#              "4": False,
+#              "5": False,
+#              "6": False,
+#              "7": False,
+#              "8": False,
+#              "9": False,
+#              "10": False,
+#              "11": False,
+#              "12": False,
+#              "13": False,
+#              "14": False,
+#              "403": False}
 #
-# propertyTypes = {"Flat": False,
-#                  "House": False,
-#                  "Penthouse": False,
-#                  "Farm House": False,
-#                  "Lower Portion": False,
-#                  "Upper Portion": False}
 #
-# bedrooms = {"0": False,
-#             "1": False,
-#             "2": False,
-#             "3": False,
-#             "4": False,
-#             "5": False,
-#             "6": False,
-#             "7": False,
-#             "8": False,
-#             "9": False,
-#             "10": False,
-#             "11": False,
-#             "12": False,
-#             "13": False,
-#             "14": False,
-#             "15": False,
-#             "16": False,
-#             "18": False,
-#             "21": False,
-#             "25": False,
-#             "27": False,
-#             "28": False,
-#             "68": False}
+# # All true dictionaries:
+# # cities = {
+# #     "Islamabad": True,
+# #     "Lahore": True,
+# #     "Faisalabad": True,
+# #     "Rawalpindi": True,
+# #     "Karachi": True
+# # }
 #
-bathrooms = {"0": True,
-             "1": False,
-             "2": False,
-             "3": False,
-             "4": False,
-             "5": False,
-             "6": False,
-             "7": False,
-             "8": False,
-             "9": False,
-             "10": False,
-             "11": False,
-             "12": False,
-             "13": False,
-             "14": False,
-             "403": False}
-
-
-# All true dictionaries:
-# cities = {
-#     "Islamabad": True,
-#     "Lahore": True,
-#     "Faisalabad": True,
-#     "Rawalpindi": True,
-#     "Karachi": True
+# propertyTypes = {
+#     "Flat": True,
+#     "House": True,
+#     "Penthouse": True,
+#     "Farm House": True,
+#     "Lower Portion": True,
+#     "Upper Portion": True
 # }
-
-propertyTypes = {
-    "Flat": True,
-    "House": True,
-    "Penthouse": True,
-    "Farm House": True,
-    "Lower Portion": True,
-    "Upper Portion": True
-}
-
-bedrooms = {
-    "0": True,
-    "1": True,
-    "2": True,
-    "3": True,
-    "4": True,
-    "5": True,
-    "6": True,
-    "7": True,
-    "8": True,
-    "9": True,
-    "10": True,
-    "11": True,
-    "12": True,
-    "13": True,
-    "14": True,
-    "15": True,
-    "16": True,
-    "18": True,
-    "21": True,
-    "25": True,
-    "27": True,
-    "28": True,
-    "68": True
-}
-
-# bathrooms = {
+#
+# bedrooms = {
 #     "0": True,
 #     "1": True,
 #     "2": True,
@@ -117,16 +91,41 @@ bedrooms = {
 #     "12": True,
 #     "13": True,
 #     "14": True,
-#     "403": True
+#     "15": True,
+#     "16": True,
+#     "18": True,
+#     "21": True,
+#     "25": True,
+#     "27": True,
+#     "28": True,
+#     "68": True
 # }
+#
+# # bathrooms = {
+# #     "0": True,
+# #     "1": True,
+# #     "2": True,
+# #     "3": True,
+# #     "4": True,
+# #     "5": True,
+# #     "6": True,
+# #     "7": True,
+# #     "8": True,
+# #     "9": True,
+# #     "10": True,
+# #     "11": True,
+# #     "12": True,
+# #     "13": True,
+# #     "14": True,
+# #     "403": True
+# # }
 
 
-df = pd.read_csv("pakistanHousingData_cleaned.csv") # Not needed here, should be in main
+df = pd.read_csv("pakistanHousingData_cleaned.csv")
 df["bedrooms"] = df["bedrooms"].astype(str)
 df["baths"] = df["baths"].astype(str)
 
 def parseSelectedData(df, cityDict, propDict, bedDict, bathDict):
-
 
     for key in cityDict:
         if cityDict[key] == False:
@@ -151,6 +150,6 @@ def parseSelectedData(df, cityDict, propDict, bedDict, bathDict):
 # Testing:
 
 
-selectedData = parseSelectedData(df, cities, propertyTypes, bedrooms, bathrooms)
-for house in selectedData.data:
-    print(house);
+# selectedData = parseSelectedData(df, cities, propertyTypes, bedrooms, bathrooms)
+# for house in selectedData.data:
+#     print(house);
