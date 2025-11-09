45e5b9051cea6f519cc92704c6474ee49f58b807
Maya Thors <mthors@ufl.edu>
2025-11-08 19:08:09 -0500

    small 26 bedroom fix

--- src/dictionaries.py
+++ src/dictionaries.py
@@ -54,5 +54,5 @@ selectedBathrooms = {"0": False,
 
 all_cities = ['Faisalabad', 'Islamabad', 'Karachi', 'Lahore', 'Rawalpindi']
 all_types = ["House", "Penthouse", "Flat", "Farm House", "Lower Portion", "Upper Portion"]
-all_bedrooms = [str(i) for i in range(0, 29)]
+all_bedrooms = [str(i) for i in range(0, 29) if i != 26]
 all_bathrooms = [str(i) for i in range(0, 15)]
